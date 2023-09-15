49a59417257061ef1a6c63817fa2819df7b7f5c0
Zuolin Cheng <zuolin8@vt.edu>
2023-09-15 02:29:52 -0400

    Delete R directory

--- R/K_Bernoulli.R
+++
@@ -1,5 +0,0 @@
-K_Bernoulli <- function(Pro_1, Pro_0, t) {
-  K_i <- log(Pro_1 * exp(t) + Pro_0)
-  Value <- sum(K_i)
-  return(Value)
-}
--- R/K_D1_Bernoulli.R
+++
@@ -1,5 +0,0 @@
-K_D1_Bernoulli <- function(Pro_1, Pro_0, t) {
-  K_D1_i <- 1 - Pro_0 / (Pro_1 * exp(t) + Pro_0)
-  Value <- sum(K_D1_i)
-  return(Value)
-}
